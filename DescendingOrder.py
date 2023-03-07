5429e06ea15ab6b2ac60a6ec998521a13b5d518f
Artur <124146015+ArturGalstyan96@users.noreply.github.com>
2023-03-07 15:36:19 +0300

    Update DescendingOrder.py

--- DescendingOrder.py
+++ DescendingOrder.py
@@ -1,2 +1,2 @@
-# def descending_order(num):
-#     return int("".join(sorted([num for num in str(num)], reverse=True)))
+def descending_order(num):
+    return int("".join(sorted([num for num in str(num)], reverse=True)))
